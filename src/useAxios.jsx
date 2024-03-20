80a5e458dc3068c52b1539d7c8283ae0b9602202
cancallmechyt <cancallmechyt@gmail.com>
2024-03-20 16:45:49 +0700

    Update Axios

--- src/useAxios.jsx
+++ src/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const useAxios = axios.create({
-    baseURL: "https://fb8a-2403-6200-88a2-c598-50cd-ad2a-98a5-8eb5.ngrok-free.app", // DatabaseUrl
+    baseURL: "https://crime-api-firebase.onrender.com/", // DatabaseUrl
 })
 
 export default useAxios
